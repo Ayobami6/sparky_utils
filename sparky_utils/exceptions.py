6b53fec971f7ce9555c800a9ddb80b6aeca84cdb
Ayobami6 <ayobamiele006@gmail.com>
2025-01-14 14:31:40 +0100

    feat: Add InternalServerExceptionHandler for improved error handling in Django models

--- sparky_utils/exceptions.py
+++ sparky_utils/exceptions.py
@@ -1,6 +1,8 @@
 import logging
 import traceback
 from rest_framework.response import Response
+from django.db import models
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -14,6 +16,46 @@ def handle_internal_server_exception() -> Response:
     traceback.print_exc()
     logger.error(traceback.format_exc())
     return Response({"status": "error", "message": "Something went wrong"}, status=500)
+
+
+class SingletonMeta(type):
+    """Single meta class"""
+
+    _instances: dict = {}
+
+    def __call__(self, *args, **kwargs):
+        if self not in self._instances:
+            instance = super().__call__(*args, **kwargs)
+            self._instances[self] = instance
+        return self._instances[self]
+
+
+class InternalServerExceptionHandler(metaclass=SingletonMeta):
+
+    def __init__(self, model_object):
+        if not isinstance(model_object, models.Model):
+            raise ValueError("model_object should be an instance of Django model")
+        self.model_object = model_object
+
+    def handle_exception(self) -> Response:
+        """Handles exception
+
+        Args:
+            exception (Exception): Exception object
+
+        Returns:
+            Response: Http response
+        """
+        stack_trace = traceback.format_exc()
+        traceback.print_exc()
+        logger.error(stack_trace)
+        self.model_object.objects.create(
+            traceback=stack_trace, timestamp=datetime.now(), severity="ERROR"
+        )
+        return Response(
+            {"status": "error", "message": "Something went wrong", "status_code": 500},
+            status=500,
+        )
 
 
 class ServiceException(Exception):
